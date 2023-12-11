9f476fcac5c67857943c8ab2a571dfc221f0dd01
anface <116490001+anface@users.noreply.github.com>
2023-12-11 12:37:24 +0200

    Delete site directory

--- site/aboutPage/about.js
+++
@@ -1,7 +0,0 @@
-function navigateToPage(pageUrl) {
-  window.location.href = pageUrl;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.classList.add('fade-in');
-});
